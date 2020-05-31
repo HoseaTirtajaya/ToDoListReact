d5a44c77285911488ced9928ac429a4db30525b5
HoseaTirtajaya <hoseatirtajaya@gmail.com>
2020-05-31 21:00:42 +0700

    Initial Commit :snowman:

---
+++ src/components/ToDoItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "../style.css";
+
+function ToDoItems() {
+  return (
+    <div className="todo-item">
+      <input type="checkbox" />
+      <span>To Do Item</span>
+    </div>
+  );
+}
+
+export default ToDoItems;
